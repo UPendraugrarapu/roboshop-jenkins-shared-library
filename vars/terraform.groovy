d4e7bf118568661b1eec7be0b7d5479151e61b1c
upendra <ugrarapuupendra@gmail.com>
2023-05-30 07:01:47 +0530

    Updated code

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -2,15 +2,22 @@ def call () {
     pipeline {
         agent any
 
+        parameters {
+            string(name : 'ENV', defaultvalue : '', descrption: 'Which environment?')
+        }
+        options {
+            ansicolor('xterm')
+        }
         stages {
             stage('init') {
                 steps {
-                    sh 'terraform init -backend-config=env-dev/state.tfvars'
+                    sh 'terraform init -backend-config=env-${ENV}/state.tfvars'
                 }
             }
             stage ('Apply') {
                 steps {
-                    sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
+                    //sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
+                    sh 'echo'
                 }
             }
         }
