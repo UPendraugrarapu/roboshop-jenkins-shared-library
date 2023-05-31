923061766ad1816d6019d12b78cf225edca0f2f8
upendra <ugrarapuupendra@gmail.com>
2023-05-31 15:57:15 +0530

    Updated code

--- vars/common.groovy
+++ vars/common.groovy
@@ -15,5 +15,5 @@ def testcases(){
   }
 
 def codequality() {
-    sh 'sonar-scanner -Dsonar.host.url=http://44.197.213.168:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component}'
+    sh 'sonar-scanner -Dsonar.host.url=http://44.197.213.168:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component} ${sonar_extra_opts}'
 }
