ce76e7b7e0668173362284728dc153582afcc9ca
upendra <ugrarapuupendra@gmail.com>
2023-06-01 08:08:20 +0530

    Updated code

--- vars/common.groovy
+++ vars/common.groovy
@@ -15,5 +15,8 @@ def testcases(){
   }
 
 def codequality() {
-    sh 'sonar-scanner -Dsonar.host.url=http://44.197.213.168:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component} ${sonar_extra_opts}'
+    withAWSParameterStore( credentialsId: 'PARAM1',naming: 'absolute', path: '/sonarqube', recursive: true, regionName: 'us-east-1') {
+        sh 'sonar-scanner -Dsonar.host.url=http://44.197.213.168:9000 -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=admin123 -Dsonar.projectKey=${component} ${sonar_extra_opts}'
+    }
+
 }
