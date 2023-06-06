0441b9494e06a4b1876eb8e431d32e4df6d69303
upendra <ugrarapuupendra@gmail.com>
2023-06-06 09:14:16 +0530

    Updated code

--- vars/ci_scripted.groovy
+++ vars/ci_scripted.groovy
@@ -36,7 +36,7 @@ def call () {
             }
             if (env.GTAG == "true" ){
                 stage('Package') {
-                    common.testcases()
+                    common.prepareArtifacts()
                 }
                 stage('Artifact Upload') {
                     common.testcases()
