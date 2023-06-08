16504e071fc49b6448aee321a0f07815415b6db9
upendra <ugrarapuupendra@gmail.com>
2023-06-08 08:09:27 +0530

    Updated code

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -4,7 +4,7 @@ def call () {
 
         parameters {
             string(name : 'ENV', defaultValue : '', description: 'Which environment?')
-            string(name : 'ACTION', defaultValue : '', description: 'Which environment?')
+            string(name : 'ACTION', defaultValue : '', description: 'Which Action?')
         }
         options {
             ansiColor('xterm')
