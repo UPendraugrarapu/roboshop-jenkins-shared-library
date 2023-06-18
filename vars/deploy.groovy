8b62147647fc58e9aa0421fa0a21fb6987568c86
upendra <ugrarapuupendra@gmail.com>
2023-06-18 11:20:22 +0530

    Updated code

---
+++ vars/deploy.groovy
@@ -0,0 +1,33 @@
+def call(){
+    pipeline{
+        agent any
+
+        parameters {
+            string(name: 'app_version', defaultValue: '', description: 'App Version')
+            string(name: 'component', defaultValue: '', description: 'Component')
+            string(name: 'environment', defaultValue: '', description: 'Environment')
+        }
+
+        stages{
+
+//            Get parameter from jenkins.io documentation
+          stage('update the parameter store'){
+            steps{
+                sh 'aws ssm put-parameter --name ${environment}.${component}.app_version --type "String" --value "${app_version}" --overwrite'
+            }
+          }
+            stage('Deploy servers'){
+                steps{
+                  sh 'aws ec2 describe-instances --filters "Name=tag:Name, Values=${component}-${environment}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text >/tmp/servers'
+                  sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment}'
+
+                }
+            }
+        }
+        post{
+            always{
+                cleansWs()
+            }
+        }
+    }
+}
